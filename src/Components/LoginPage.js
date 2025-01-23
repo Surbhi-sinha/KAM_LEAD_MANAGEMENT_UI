4bfe845b02adf4aa24ca6caff1c3e6cafefbfd70
Surbhi sinha <astsurbhisinha@gmail.com>
2025-01-23 23:02:45 +0530

    modified signup login UI

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -2,8 +2,13 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import NavigationBar from "./Navbar";
+import loginimage from '../Images/loginimage.png';
+import success from '../Images/suceess.gif';
+import '../styles/Auth.css'
 
 const Login = () => {
+
+      const [showSuccess, setShowSuccess] = useState(false);
       const navigate = useNavigate();
       const [formData, setFormData] = useState({
             username: "",
@@ -22,9 +27,14 @@ const Login = () => {
             try {
                   const response = await axios.post("http://localhost:5000/api/auth/signin", formData);
                   console.log(response.data);
-                  alert(response.data.message);
+                  // alert(response.data.message);
                   localStorage.setItem('user', JSON.stringify({ username: response.data.username }))
-                  navigate("/");
+
+                  setShowSuccess(true);
+                  setTimeout(() => {
+                        setShowSuccess(false)
+                        navigate("/");
+                  }, 3000);
             } catch (err) {
                   console.log(err);
                   let value = err.response.data.message
@@ -36,52 +46,65 @@ const Login = () => {
             <div>
                   <NavigationBar />
 
-                  <div className="container mt-5 card w-50" >
-                        <h2 className="card-header mt-3 mb-5">Login</h2>
-                        <div>
 
-                              <img
-                                    src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-                                    alt="profile-img"
-                                    className="profile-img-card mt-5 mb-5 rounded-circle img-fluid w-25 "
-                              />
+                  <div className="container">
+
+                        <div className="row bkg-transparent-dark m-4 mt-5 mb-5 p-4 rounded-3">
+                              <div className="col-md-5 ">
+
+                                    <div className="mt-5 card bg-transparent border-0 lexend-Font light-text" >
+                                          <h2 className="card-header mt-3 mb-5">Welcome Back!</h2>
+
+                                          <p className="mb-5">"Step Into Your Success – Log In to Seamlessly Manage Leads, Boost Productivity, and Drive Your Business Forward! 🚀✨"</p>
+                                          <form onSubmit={handleSubmit}>
+                                                <div className="mb-3 row">
+                                                      <label htmlFor="username" className="col-sm-3 col-form-label">Username</label>
+                                                      <div className="col-sm-8">
+                                                            <input
+                                                                  type="text"
+                                                                  className="form-control"
+                                                                  id="username"
+                                                                  name="username"
+                                                                  value={formData.username}
+                                                                  onChange={handleInputChange}
+                                                                  required
+                                                            />
+                                                      </div>
+                                                </div>
+                                                <div className="mb-3 row">
+                                                      <label htmlFor="password" className="col-sm-3 col-form-label">Password</label>
+                                                      <div className="col-sm-8">
+                                                            <input
+                                                                  type="password"
+                                                                  className="form-control"
+                                                                  id="password"
+                                                                  name="password"
+                                                                  value={formData.password}
+                                                                  onChange={handleInputChange}
+                                                                  required
+                                                            />
+                                                      </div>
+                                                </div>
+                                                <div className="row mb-3">
+                                                      <div className="col-sm-8 offset-sm-2">
+                                                            <button type="submit" className="btn button-light hover-effect">Login</button>
+                                                      </div>
+                                                </div>
+                                          </form>
+
+                                          {/* Success GIF Modal */}
+                                          {showSuccess && (
+                                                <div className="success-modal">
+                                                      <img src={success} alt="Success" className="success-gif" />
+                                                </div>
+                                          )}
+
+                                    </div>
+                              </div>
+                              <div className="col-md-7">
+                                    <img src={loginimage} alt="Description of the SVG image" height="600" />
+                              </div>
                         </div>
-                        <form onSubmit={handleSubmit}>
-                              <div className="mb-3 row">
-                                    <label htmlFor="username" className="col-sm-3 col-form-label">Username</label>
-                                    <div className="col-sm-8">
-                                          <input
-                                                type="text"
-                                                className="form-control"
-                                                id="username"
-                                                name="username"
-                                                value={formData.username}
-                                                onChange={handleInputChange}
-                                                required
-                                          />
-                                    </div>
-                              </div>
-                              <div className="mb-3 row">
-                                    <label htmlFor="password" className="col-sm-3 col-form-label">Password</label>
-                                    <div className="col-sm-8">
-                                          <input
-                                                type="password"
-                                                className="form-control"
-                                                id="password"
-                                                name="password"
-                                                value={formData.password}
-                                                onChange={handleInputChange}
-                                                required
-                                          />
-                                    </div>
-                              </div>
-                              <div className="row mb-3">
-                                    <div className="col-sm-8 offset-sm-2">
-                                          <button type="submit" className="btn btn-primary">Login</button>
-                                    </div>
-                              </div>
-                        </form>
-
                   </div>
             </div>
       );
--- src/Components/SignupPage.js
+++ src/Components/SignupPage.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import NavigationBar from "./Navbar";
-import { isEmail } from "validator"
+import Signupgif from '../Images/signimage.svg';
+import 'animate.css'; // Import Animate.css;
+import '../styles/Auth.css';
+
 const Signup = () => {
+      const [showSuccess, setShowSuccess] = useState(false);
+
+
       const navigate = useNavigate();
       const [formData, setFormData] = useState({
             username: "",
@@ -22,8 +27,10 @@ const Signup = () => {
             // Add logic to handle signup with backend
             try {
                   const response = await axios.post("http://localhost:5000/api/auth/signup", formData);
-                  alert(response.data.message);
-                  navigate("/");
+                  // alert(response.data.message);
+                  setShowSuccess(true);
+                  setTimeout(() => { setShowSuccess(false) }, 2000)
+                  // navigate("/");
             } catch (err) {
                   console.log(err);
                   alert('Error registering User :', err)
@@ -31,73 +38,83 @@ const Signup = () => {
       };
 
       return (
-            <div>
-                  <NavigationBar />
+            <div className="p-4">
+                  <div className="container mt-4">
+                        <div className="bkg-transparent-dark m-4 mt-5 mb-5 p-4 rounded-3">
+                              <div className="row justify-content-center">
+                                    <div className="col-md-6">
+                                          <img src={Signupgif} alt="Description of the SVG image" height="600" />
+                                    </div>
+                                    <div className="col-md-6">
+                                          <div className="container mt-5 card bg-transparent lexend-Font light-text border-0 p-2" >
+                                                <h2 className="card-header mt-3 mb-5">Welcome to KAM</h2>
+                                                <p className="mb-5">"Streamline Your Leads, Simplify Your Success – Sign Up Today and Take Control of Your Business Growth! 🚀"</p>
+                                                <form onSubmit={handleSubmit}>
+                                                      <div className="mb-3 row">
+                                                            <label htmlFor="username" className="col-sm-3 col-form-label fw-bold">Username</label>
+                                                            <div className="col-sm-8">
+                                                                  <input
+                                                                        type="text"
+                                                                        className="form-control"
+                                                                        id="username"
+                                                                        name="username"
+                                                                        value={formData.username}
+                                                                        onChange={handleInputChange}
+                                                                        required
+                                                                  />
+                                                            </div>
+                                                      </div>
+                                                      <div className="mb-3 row">
+                                                            <label htmlFor="email" className="col-sm-3 col-form-label fw-bold">Email</label>
+                                                            <div className="col-sm-8">
+                                                                  <input
+                                                                        type="email"
+                                                                        className="form-control"
+                                                                        id="email"
+                                                                        name="email"
+                                                                        value={formData.email}
+                                                                        onChange={handleInputChange}
+                                                                        required
+                                                                  />
+                                                            </div>
+                                                      </div>
+                                                      <div className="mb-3 row">
+                                                            <label htmlFor="password" className="col-sm-3 col-form-label fw-bold">Password</label>
+                                                            <div className="col-sm-8">
+                                                                  <input
+                                                                        type="password"
+                                                                        className="form-control"
+                                                                        id="password"
+                                                                        name="password"
+                                                                        value={formData.password}
+                                                                        onChange={handleInputChange}
+                                                                        required
+                                                                  />
+                                                            </div>
+                                                      </div>
+                                                      <div className="row">
+                                                            <div className="col-sm-8 offset-sm-2">
+                                                                  <button type="submit" className="btn button-light hover-effect">Register Now</button>
+                                                            </div>
+                                                      </div>
 
-                  <div className="container mt-5 card w-50" >
-                        <h2 className="card-header mt-3 mb-5">Sign Up</h2>
-                        <div>
+                                                      {/* Success Message with Animate.css */}
+                                                      {showSuccess && (
+                                                            <div className="alert alert-success animate__animated animate__bounceInRight">
+                                                                  Sign In Successful! 🎉
+                                                            </div>
+                                                      )}
+                                                </form>
 
-                              <img
-                                    src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-                                    alt="profile-img"
-                                    className="profile-img-card mt-5 mb-5 rounded-circle img-fluid w-25 "
-                              />
+                                                <p className="mt-3">
+                                                      Already have an account? <Link to="/login"><span className="light-text">Login here</span></Link>
+                                                </p>
+                                          </div>
+                                    </div>
+                              </div>
                         </div>
-                        <form onSubmit={handleSubmit}>
-                              <div className="mb-3 row">
-                                    <label htmlFor="username" className="col-sm-3 col-form-label fw-bold">Username</label>
-                                    <div className="col-sm-8">
-                                          <input
-                                                type="text"
-                                                className="form-control"
-                                                id="username"
-                                                name="username"
-                                                value={formData.username}
-                                                onChange={handleInputChange}
-                                                required
-                                          />
-                                    </div>
-                              </div>
-                              <div className="mb-3 row">
-                                    <label htmlFor="email" className="col-sm-3 col-form-label fw-bold">Email</label>
-                                    <div className="col-sm-8">
-                                          <input
-                                                type="email"
-                                                className="form-control"
-                                                id="email"
-                                                name="email"
-                                                value={formData.email}
-                                                onChange={handleInputChange}
-                                                required
-                                          />
-                                    </div>
-                              </div>
-                              <div className="mb-3 row">
-                                    <label htmlFor="password" className="col-sm-3 col-form-label fw-bold">Password</label>
-                                    <div className="col-sm-8">
-                                          <input
-                                                type="password"
-                                                className="form-control"
-                                                id="password"
-                                                name="password"
-                                                value={formData.password}
-                                                onChange={handleInputChange}
-                                                required
-                                          />
-                                    </div>
-                              </div>
-                              <div className="row">
-                                    <div className="col-sm-8 offset-sm-2">
-                                          <button type="submit" className="btn btn-primary">Register Now</button>
-                                    </div>
-                              </div>
-                        </form>
-
-                        <p className="mt-3">
-                              Already have an account? <Link to="/login">Login here</Link>
-                        </p>
                   </div>
+
             </div>
       );
 };
